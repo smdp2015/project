39fa225a5f6631048265e1567b45acfcb4189539
Morten Egelund Rasmussen <morten.egelund.rasmussen@gmail.com>
2015-05-08 21:38:37 +0200

    Added grammar tests.

--- dk.itu.smdp2015.church.configurator.syntax.tests/src/dk/itu/smdp2015/church/configurator/syntax/tests/ConfiguratorGrammarTest.xtend
+++ dk.itu.smdp2015.church.configurator.syntax.tests/src/dk/itu/smdp2015/church/configurator/syntax/tests/ConfiguratorGrammarTest.xtend
@@ -12,23 +12,110 @@ import dk.itu.smdp2015.church.ConfiguratorInjectorProvider
 import dk.itu.smdp2015.church.model.configurator.ConfiguratorPackage
 import org.junit.Before
 
-@RunWith(typeof(XtextRunner))
-@InjectWith(typeof(ConfiguratorInjectorProvider))
-class MyTest {
+import static org.junit.Assert.*
+import dk.itu.smdp2015.church.model.configurator.Parameter
+import dk.itu.smdp2015.church.model.configurator.Bounded
+import dk.itu.smdp2015.church.model.configurator.impl.IntegerImpl
+import dk.itu.smdp2015.church.validation.ConfiguratorValidator
+import dk.itu.smdp2015.church.model.configurator.Enumerated
+import dk.itu.smdp2015.church.model.configurator.impl.StringImpl
+import org.eclipse.xtext.diagnostics.Diagnostic
+import dk.itu.smdp2015.church.model.configurator.Constraint
+import dk.itu.smdp2015.church.model.configurator.Binary
+import dk.itu.smdp2015.church.model.configurator.BinaryOperator
+import dk.itu.smdp2015.church.model.configurator.Identifier
+
+@RunWith(XtextRunner)
+@InjectWith(ConfiguratorInjectorProvider)
+class ConfiguratorGrammarTest {
 	
-	@Inject
-	extension ParseHelper<Configurator>
+	@Inject extension ParseHelper<Configurator>
 	
-	@Inject
-	extension ValidationTestHelper
+	@Inject extension ValidationTestHelper
 	
 	@Before
 	def void before() {
 		ConfiguratorPackage.eINSTANCE.eClass		
 	}
+
+	@Test
+	def void testInvalidModelNoParameters() {
+		var model = '''configurator Empty'''.parse
+		model.assertError(ConfiguratorPackage.Literals.CONFIGURATOR, Diagnostic.SYNTAX_DIAGNOSTIC, "mismatched input")
+	}
 	
 	@Test
-	def void testFoo() {
-		'''configurator car { parameter foo values [2;3] }'''.parse.assertNoErrors
+	def void testValidBoundedRange() {
+		var model = '''configurator Bicycle "Bicycle configuration" { parameter wheel_size values [16;24] }'''.parse
+		assertEquals("Bicycle configuration", model.description)
+		var param = model.parameters.get(0) as Parameter
+		assertEquals("wheel_size", param.name)
+		var valueRange = param.valueRange as Bounded
+		assertEquals(16, (valueRange.lowerBound as IntegerImpl).value)
+		assertEquals(24, (valueRange.upperBound as IntegerImpl).value)
+		model.assertNoErrors
 	}
+
+	@Test
+	def void testInvalidBoundedRangeReverse() {
+		var model = '''configurator Bicycle { parameter wheel_size values [24;16] }'''.parse
+		model.assertError(ConfiguratorPackage.Literals.BOUNDED, ConfiguratorValidator.INVALID_BOUND, "Lower bound should be less than upper bound")
+	}
+
+	@Test
+	def void testInvalidBoundedRangeWrongTypes() {
+		var model = '''configurator Bicycle { parameter wheel_size values [b;16] }'''.parse
+		model.assertError(ConfiguratorPackage.Literals.BOUNDED, ConfiguratorValidator.WRONG_TYPE, "expected the same type")
+	}
+
+	@Test
+	def void testValidEnum() {
+		var model = '''configurator Car { parameter Variant values ("Standard", "Sport", "Luxury") }'''.parse
+		assertNull(model.description)
+		var param = model.parameters.get(0) as Parameter
+		assertEquals("Variant", param.name)
+		assertFalse(param.optional)
+		var enumerated = param.valueRange as Enumerated
+		assertEquals(3, enumerated.values.length)
+		assertEquals("Standard", (enumerated.values.get(0) as StringImpl).value)
+		assertEquals("Sport", (enumerated.values.get(1) as StringImpl).value)
+		assertEquals("Luxury", (enumerated.values.get(2) as StringImpl).value)
+		model.assertNoErrors
+	}
+
+	@Test
+	def void testInvalidEnumWrongElement() {
+		var model = '''configurator Car { parameter Variant values ("Standard", "Sport", Luxury) }'''.parse
+		model.assertError(ConfiguratorPackage.Literals.ENUMERATED, ConfiguratorValidator.INVALID_ENUMERATION, "Enumerated item should be a constant")
+	}
+
+ /* 
+	@Test
+	def void testConstraints() {
+		var model = '''
+			configurator car "A configurator for a car" {
+			parameter color "the seat color" values ("red", "blue", "black"),
+			parameter seat_material "the seat material" values ("leather", "cloth")
+			constraints
+			{
+				description "invalid seats: leather can only have colour red, black" seat_material != "leather" or (color in ("red", "black")),
+				description "invalid seats: cloth can not be red" seat_material != "cloth" or color in ("blue", "black")
+			}
+			}
+        '''.parse
+		//assertEquals("A configurator for a car", model.description)
+		//var param = model.parameters.get(1) as Parameter
+		//assertEquals("the seat material", param.description)
+		//assertEquals(2, param.constraints.length)
+		//var constraint1 = param.constraints.get(0) as Constraint
+		//assertEquals("invalid seats: leather can only have colour red, black", constraint1.description)
+		//var exp1 = constraint1.expression as Binary
+		//var exp2 = exp1.left as Binary
+		//assertEquals(BinaryOperator.NOT_EQUAL, exp2.operator)
+		//assertEquals(param, (exp2.left as Identifier).id)
+		//assertEquals("leather", (exp2.right as StringImpl).value)
+		//model.assertNoErrors
+	}
+*/
+
 }
---
+++ dk.itu.smdp2015.church.configurator.syntax.tests/xtend-gen/dk/itu/smdp2015/church/configurator/syntax/tests/ConfiguratorGrammarTest.java
@@ -0,0 +1,160 @@
+package dk.itu.smdp2015.church.configurator.syntax.tests;
+
+import dk.itu.smdp2015.church.ConfiguratorInjectorProvider;
+import dk.itu.smdp2015.church.model.configurator.AbstractParameter;
+import dk.itu.smdp2015.church.model.configurator.Bounded;
+import dk.itu.smdp2015.church.model.configurator.Configurator;
+import dk.itu.smdp2015.church.model.configurator.ConfiguratorPackage;
+import dk.itu.smdp2015.church.model.configurator.Enumerated;
+import dk.itu.smdp2015.church.model.configurator.Expression;
+import dk.itu.smdp2015.church.model.configurator.Parameter;
+import dk.itu.smdp2015.church.model.configurator.ValueRange;
+import dk.itu.smdp2015.church.model.configurator.impl.IntegerImpl;
+import dk.itu.smdp2015.church.model.configurator.impl.StringImpl;
+import dk.itu.smdp2015.church.validation.ConfiguratorValidator;
+import javax.inject.Inject;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.diagnostics.Diagnostic;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.eclipse.xtext.junit4.validation.ValidationTestHelper;
+import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(XtextRunner.class)
+@InjectWith(ConfiguratorInjectorProvider.class)
+@SuppressWarnings("all")
+public class ConfiguratorGrammarTest {
+  @Inject
+  @Extension
+  private ParseHelper<Configurator> _parseHelper;
+  
+  @Inject
+  @Extension
+  private ValidationTestHelper _validationTestHelper;
+  
+  @Before
+  public void before() {
+    ConfiguratorPackage.eINSTANCE.eClass();
+  }
+  
+  @Test
+  public void testInvalidModelNoParameters() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("configurator Empty");
+      Configurator model = this._parseHelper.parse(_builder);
+      this._validationTestHelper.assertError(model, ConfiguratorPackage.Literals.CONFIGURATOR, Diagnostic.SYNTAX_DIAGNOSTIC, "mismatched input");
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  @Test
+  public void testValidBoundedRange() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("configurator Bicycle \"Bicycle configuration\" { parameter wheel_size values [16;24] }");
+      Configurator model = this._parseHelper.parse(_builder);
+      String _description = model.getDescription();
+      Assert.assertEquals("Bicycle configuration", _description);
+      EList<AbstractParameter> _parameters = model.getParameters();
+      AbstractParameter _get = _parameters.get(0);
+      Parameter param = ((Parameter) _get);
+      String _name = param.getName();
+      Assert.assertEquals("wheel_size", _name);
+      ValueRange _valueRange = param.getValueRange();
+      Bounded valueRange = ((Bounded) _valueRange);
+      Expression _lowerBound = valueRange.getLowerBound();
+      int _value = ((IntegerImpl) _lowerBound).getValue();
+      Assert.assertEquals(16, _value);
+      Expression _upperBound = valueRange.getUpperBound();
+      int _value_1 = ((IntegerImpl) _upperBound).getValue();
+      Assert.assertEquals(24, _value_1);
+      this._validationTestHelper.assertNoErrors(model);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  @Test
+  public void testInvalidBoundedRangeReverse() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("configurator Bicycle { parameter wheel_size values [24;16] }");
+      Configurator model = this._parseHelper.parse(_builder);
+      this._validationTestHelper.assertError(model, ConfiguratorPackage.Literals.BOUNDED, ConfiguratorValidator.INVALID_BOUND, "Lower bound should be less than upper bound");
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  @Test
+  public void testInvalidBoundedRangeWrongTypes() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("configurator Bicycle { parameter wheel_size values [b;16] }");
+      Configurator model = this._parseHelper.parse(_builder);
+      this._validationTestHelper.assertError(model, ConfiguratorPackage.Literals.BOUNDED, ConfiguratorValidator.WRONG_TYPE, "expected the same type");
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  @Test
+  public void testValidEnum() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("configurator Car { parameter Variant values (\"Standard\", \"Sport\", \"Luxury\") }");
+      Configurator model = this._parseHelper.parse(_builder);
+      String _description = model.getDescription();
+      Assert.assertNull(_description);
+      EList<AbstractParameter> _parameters = model.getParameters();
+      AbstractParameter _get = _parameters.get(0);
+      Parameter param = ((Parameter) _get);
+      String _name = param.getName();
+      Assert.assertEquals("Variant", _name);
+      boolean _isOptional = param.isOptional();
+      Assert.assertFalse(_isOptional);
+      ValueRange _valueRange = param.getValueRange();
+      Enumerated enumerated = ((Enumerated) _valueRange);
+      EList<Expression> _values = enumerated.getValues();
+      int _length = ((Object[])Conversions.unwrapArray(_values, Object.class)).length;
+      Assert.assertEquals(3, _length);
+      EList<Expression> _values_1 = enumerated.getValues();
+      Expression _get_1 = _values_1.get(0);
+      String _value = ((StringImpl) _get_1).getValue();
+      Assert.assertEquals("Standard", _value);
+      EList<Expression> _values_2 = enumerated.getValues();
+      Expression _get_2 = _values_2.get(1);
+      String _value_1 = ((StringImpl) _get_2).getValue();
+      Assert.assertEquals("Sport", _value_1);
+      EList<Expression> _values_3 = enumerated.getValues();
+      Expression _get_3 = _values_3.get(2);
+      String _value_2 = ((StringImpl) _get_3).getValue();
+      Assert.assertEquals("Luxury", _value_2);
+      this._validationTestHelper.assertNoErrors(model);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  @Test
+  public void testInvalidEnumWrongElement() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("configurator Car { parameter Variant values (\"Standard\", \"Sport\", Luxury) }");
+      Configurator model = this._parseHelper.parse(_builder);
+      this._validationTestHelper.assertError(model, ConfiguratorPackage.Literals.ENUMERATED, ConfiguratorValidator.INVALID_ENUMERATION, "Enumerated item should be a constant");
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+}
